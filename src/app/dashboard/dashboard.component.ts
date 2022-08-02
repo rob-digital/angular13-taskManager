c042fe78b016fe3c2bf91d685705093e6c2cebce
rokselar <robert.roksela@gmail.com>
2022-08-02 10:19:06 +0100

    Angular App started, ngIf, ngFor done

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
+
+  Clients: string[] = []
+  Projects: any = []
+  Years: number[] = []
+  TeamMembersSummary: any = []
+  TeamMembers: any = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.Designation = 'Team Leader';
+    this.Username = 'Scott Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+
+    this.Clients = [
+      'ABC Infotech Ltd.',
+      'DEF Software Solutions',
+      'GHI Industries',
+    ];
+
+    this.Projects = [
+      {name: 'Project A'},
+      {name: 'Project B'},
+      {name: 'Project C'},
+      {name: 'Project D'},
+    ];
+
+    for (let i = 2022; i > 2012; i--) {
+      this.Years.push(i)
+    }
+
+    this.TeamMembersSummary = [
+      {
+        Region: 'East',
+        TeamMembersCount: 20,
+        TemporarilyUnavailableMembers: 4,
+      },
+      {
+        Region: 'West',
+        TeamMembersCount: 15,
+        TemporarilyUnavailableMembers: 8,
+      },
+      {
+        Region: 'South',
+        TeamMembersCount: 17,
+        TemporarilyUnavailableMembers: 1,
+      },
+      {
+        Region: 'North',
+        TeamMembersCount: 15,
+        TemporarilyUnavailableMembers: 6,
+      },
+    ];
+
+
+  this.TeamMembers = [
+    {
+      Region: 'East',
+      Members: [
+        { ID: 1, Name: 'Ford', Status: 'Available' },
+        { ID: 2, Name: 'Miller', Status: 'Available' },
+        { ID: 3, Name: 'Jones', Status: 'Busy' },
+        { ID: 4, Name: 'James', Status: 'Busy' },
+      ],
+    },
+    {
+      Region: 'West',
+      Members: [
+        { ID: 5, Name: 'Anna', Status: 'Available' },
+        { ID: 6, Name: 'Arun', Status: 'Available' },
+        { ID: 7, Name: 'Varun', Status: 'Busy' },
+        { ID: 8, Name: 'Jasmine', Status: 'Busy' },
+      ],
+    },
+    {
+      Region: 'South',
+      Members: [
+        { ID: 9, Name: 'Krishna', Status: 'Available' },
+        { ID: 10, Name: 'Mohan', Status: 'Available' },
+        { ID: 11, Name: 'Raju', Status: 'Busy' },
+        { ID: 12, Name: 'Farooq', Status: 'Busy' },
+      ],
+    },
+    {
+      Region: 'North',
+      Members: [
+        { ID: 13, Name: 'Jacob', Status: 'Available' },
+        { ID: 14, Name: 'Smith', Status: 'Available' },
+        { ID: 15, Name: 'Jones', Status: 'Busy' },
+        { ID: 16, Name: 'James', Status: 'Busy' },
+      ],
+    },
+  ];
+}
+
+onProjectChange($event: any)
+  {
+    if ($event.target.innerHTML.trim() == 'Project A')
+    {
+      this.ProjectCost = 2113507;
+      this.CurrentExpenditure = 96788;
+      this.AvailableFunds = 52436;
+    } else if ($event.target.innerHTML.trim() == 'Project B')
+    {
+      this.ProjectCost = 88923;
+      this.CurrentExpenditure = 22450;
+      this.AvailableFunds = 2640;
+    } else if ($event.target.innerHTML.trim() == 'Project C')
+    {
+      this.ProjectCost = 662183;
+      this.CurrentExpenditure = 7721;
+      this.AvailableFunds = 9811;
+    } else if ($event.target.innerHTML.trim() == 'Project D')
+    {
+      this.ProjectCost = 928431;
+      this.CurrentExpenditure = 562;
+      this.AvailableFunds = 883;
+    }
+  }
+}
